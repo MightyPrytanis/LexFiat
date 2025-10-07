d45dc165e68d733b5f7527bea9002d103ac06456
MightyPrytanis <davidtowne@gmail.com>
2025-10-07 01:54:22 -0400

    chore: bump build (build-bump comment) to force new asset hashes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,4 +3,5 @@ import App from "./App";
 import "./index.css";
 import "./styles/piquette.css";
 
+// build-bump: 2025-10-07T05:55:00Z - render rebuild trigger
 createRoot(document.getElementById("root")!).render(<App />);
